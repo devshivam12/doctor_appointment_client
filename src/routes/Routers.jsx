c7835cfc348f2eb39ee63086596dd8822cb07b39
Shivam <mittalshivam440@gmail.com>
2024-07-07 18:46:46 +0530

    initilization

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,34 +1,71 @@
-import React, { Suspense, lazy } from 'react'
-import Home from '../pages/Home'
-const Service = lazy(() => import('../pages/Service'))
-const Login = lazy(() => import('../pages/Login'))
-const Signup = lazy(() => import('../pages/Signup'))
-const Contact = lazy(() => import('../pages/Contact'))
-const Doctors = lazy(() => import('../pages/doctor/Doctors'))
-const DoctorDetails = lazy(() => import('../pages/doctor/DoctorDetails'))
+import React, { Suspense, lazy } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 
-import LoadingGif from '../component/helper/LoadingGif'
+const Home = lazy(() => import('../pages/Home'));
+const Service = lazy(() => import('../pages/Service'));
+const Login = lazy(() => import('../pages/Login'));
+const Signup = lazy(() => import('../pages/Signup'));
+const Contact = lazy(() => import('../pages/Contact'));
+const Doctors = lazy(() => import('../pages/doctor/Doctors'));
+const DoctorDetails = lazy(() => import('../pages/doctor/DoctorDetails'));
 
-import { Routes, Route } from 'react-router-dom'
+import LoadingGif from '../component/helper/LoadingGif';
 
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    y: 50,
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+  },
+  out: {
+    opacity: 0,
+    y: -50,
+  },
+};
+
+const pageTransition = {
+  type: 'tween',
+  ease: 'anticipate',
+  duration: 0.5,
+};
 
 const Routers = () => {
+  const location = useLocation();
+
   return (
     <>
       <Suspense fallback={<div><LoadingGif /></div>}>
-        <Routes >
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/doctors' element={<Doctors />} />
-          <Route path='/doctors/:id' element={<DoctorDetails />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Signup />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/services' element={<Service />} />
-        </Routes>
+        <AnimatePresence mode='wait'>
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+            <Route path="/home" element={<PageWrapper><Home /></PageWrapper>} />
+            <Route path="/doctors" element={<PageWrapper><Doctors /></PageWrapper>} />
+            <Route path="/doctors/:id" element={<PageWrapper><DoctorDetails /></PageWrapper>} />
+            <Route path="/login" element={<PageWrapper><Login /></PageWrapper>} />
+            <Route path="/register" element={<PageWrapper><Signup /></PageWrapper>} />
+            <Route path="/contact" element={<PageWrapper><Contact /></PageWrapper>} />
+            <Route path="/services" element={<PageWrapper><Service /></PageWrapper>} />
+          </Routes>
+        </AnimatePresence>
       </Suspense>
     </>
-  )
-}
+  );
+};
 
-export default Routers
+const PageWrapper = ({ children }) => (
+  <motion.div
+    initial="initial"
+    animate="in"
+    exit="out"
+    variants={pageVariants}
+    transition={pageTransition}
+  >
+    {children}
+  </motion.div>
+);
+
+export default Routers;
